ebb44525faf062006615cce5de94ad62ca21f8b7
Egor Talovikov <egtalovikov@gmail.com>
2022-11-09 05:22:17 +0300

    feat: сверстал хедер Home и прописал получение информации о пользователе

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -3,14 +3,32 @@ import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
-import {Sidebar, UserProfile} from '../components';
+import { Sidebar, UserProfile } from '../components';
+import Pins from './Pins'
 import { client } from '../client';
 import logo from '../assets/logo.png'
 
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
+  useEffect(() => {
+
+  }, [])
+  
+
   return (
-    <div>
-      Home
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md: flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+      <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+      <Link to={`user-profile/${user?._id}`}>
+      <img src={logo} alt="logo" className="w-28"/>
+      </Link>
+      </div>
     </div>
   )
 }
